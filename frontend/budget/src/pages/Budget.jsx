b66b83e2cbca98804ad93cc76021b0f3e509a75a
Blend <blendiishala0@gmail.com>
2024-11-06 03:31:05 +0100

    fetch

--- frontend/budget/src/pages/Budget.jsx
+++ frontend/budget/src/pages/Budget.jsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Navbar from '../components/Navbar'
+import axios from "axios"
 
 function Budget() {
+  const[budgets,setBudgets]=useState([]);
+
+
+
+  useEffect(()=>{
+    const fetchBudget=async()=>{
+     try{
+      const response=await axios.get('http://localhost:5000/api/budgets');
+      setBudgets(response.data)
+     }catch(error){
+      console.error("error fetching",error)
+     }
+    };
+    fetchBudget()
+  },[]);
+
+
+
+
+
   return (
-    <div>Budget</div>
+    <div>
+      <Navbar/>
+      <ul>
+        {budgets.map(budget=>(
+          <li key={budget.budget_id}>{budget.budget_amount} {budget.month} {budget.year}</li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
